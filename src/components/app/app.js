612c2aff54bd9e9ff69d3da943ae64f7f0642cc2
duchevich <duchevich274@gmail.com>
2019-08-20 23:58:15 +0300

    les 41

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
+    maxId = 100;
 
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 }
-    ];
+    state={
+        todoData: [
+            { label: 'Drink Coffee', important: false, id: 1 },
+            { label: 'Make Awesome App', important: true, id: 2 },
+            { label: 'Have a lunch', important: false, id: 3 }
+        ]
+    }
 
-    return (
-        <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
-        <div className="top-panel d-flex">
-            <SearchPanel />
-            <ItemStatusFilter />
-        </div>
+    deleteItem = (id) => {
+        this.setState(({ todoData }) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+            const newArr = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+            return {
+                todoData: newArr
+            }
 
-        <TodoList todos={todoData} />
-        </div>
-    );
+        });
+    }
+
+    addItem=(text) => {
+
+        const newItem = { 
+            label: text, 
+            important: false, 
+            id: this.maxId++
+        }
+        this.setState(({ todoData }) => {
+            const newArr = [...todoData, newItem];
+            return {
+                todoData: newArr
+            }
+        })
+        console.log('add ', text);
+    }
+
+    render(){
+        return (
+            <div className="todo-app">
+            <AppHeader toDo={1} done={3} />
+            <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
+
+            <TodoList 
+            todos={this.state.todoData} 
+            onDeleted={this.deleteItem}/>
+            <ItemAddForm
+            onAdd={this.addItem}/>
+            </div>
+        );
+    }
 };
-
-export default App;
